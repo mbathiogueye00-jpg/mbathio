6b15ff2cf102d41d2eccc657ac9e419a8ad587ac
mbathiogueye00-jpg <mbathiogueye00@gmail.com>
2026-02-26 17:07:55 +0000

    Update TP1CV.py

--- TP1CV.py
+++ TP1CV.py
@@ -11,7 +11,7 @@ with st.sidebar:
 
     st.write("-2026:BTS en Geomatique")
 
-st.header("Technicien supérieur en Geomatique")
+st.header("Technicienne supérieur en Geomatique")
 
 st.subheader("A propos de moi")
 st.write("Passionné par  la Géomatique ,Topographie et les SIG, je suis rigoureuse,organisée et motivée pour des projet d'amenagement du territoire")
@@ -22,9 +22,6 @@ st.write("Projection cartographique: Collecte de données spatiales sur le terra
 st.write("Base de données:  création d'une base de données spatiales")
 st.write("Analyse cartographique: pour l'amenagement du territoire")
 
-st.subheader("Expériences")
-st.write("Poste d'adjoint president de la communication de l'amical des etudiants thièsois")
-st.write("Poste de commissaire au compte dans un GIE nommé OJEUF")
 
 
 st.subheader("Compétences")
@@ -32,13 +29,14 @@ st.write("Conception de base de données sur QUANTUM GIS et ARCGIS")
 st.write("Concevoir une base de donnée sur POWER AMC, Post gres")
 st.write("Géoreferencement de carte")
 st.write("Numérisation")
-st.write("Maitrise des techniques de collecte données:application Mobile Topographer,Kobo Toolbox")
-st.write("Conception  de Plans sur Autocad")
-st.write("Maitrise avancée du Pack Office (EXCEL pour le traitement de données topographiques, WORD pour la réedaction de rapports technique, POWER POINT pour la Presentation de projets")
+st.write("Maitrise des techniques de collecte données:application Mobile Topographer,Locus Map,QField")
+st.write("Conception  de Plans sur Autocad ,sketchup")
+st.write(" Pack Office (EXCEL , WORD , POWER POINT")
 
 st.subheader("Perspectives")
-st.write("Ambitionne de créer un cabinet d'expertise en Géomatique spécialisé dans L'Amenagement du territoire")
-st.write("Ambitionne de concevoir et d'administrer des bases de données spatiales (post gis) pour l'analyse de données geographiques")
-st.write("Specialiste SiG")
+st.write("Je souhaite devenir spécialiste en Systèmes d’Information Géographique SIG et en gestion des données spatiales.")
+
+
+
 
 
