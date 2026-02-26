79e3017e4cbed1e7d19ce194d1f6dc68b0a90475
mbathiogueye00-jpg <mbathiogueye00@gmail.com>
2026-02-26 16:20:44 +0000

    Update TP1CV.py

---
+++ TP1CV.py
@@ -0,0 +1,42 @@
+import streamlit as st
+with st.sidebar:
+   
+    st.write("Papa Demba DIOP")
+    st.write("CONTACTS")
+    st.write("Email: dpapademba59@gmail.com")
+    st.write("Tel: 78147739")
+    st.write("Adresse: Medina,rue 06")
+    st.write("FORMATIONS")
+    st.write("2021-2022: BAC L2")
+    st.write("2022-2025: Licence en Geographie")
+    st.write("2025-2026:BTS en Geomatique")
+
+st.header("Technicien supérieur en Geomatique")
+
+st.subheader("A propos de moi")
+st.write("Passionné par la Geographie, la Géomatique et la Cartographie, je suis une personne rigoureuse dotée d'une capacité de travailler en equipe pour mener à bien des projets")
+
+st.subheader("Projets académiques")
+st.write("Programmation: Réalisation d'un CV digital sur Steamlit")
+st.write("Projection cartographique: Collecte de données aux environs de la Mosquée Masalik djinane")
+st.write("Base de données: Georeferencement d'une carte, création d'une base de données spatiales")
+st.write("Technique de base de la Géographie: réalisation de cartes ( themeatiques et de situation)")
+
+st.subheader("Expériences")
+st.write("Poste d'adjoint president de la communication de l'amical des etudiants thièsois")
+st.write("Poste de commissaire au compte dans un GIE nommé OJEUF")
+
+
+st.subheader("Compétences")
+st.write("Conception de base de données sur QUANTUM GIS et ARCGIS")
+st.write("Concevoir une base de donnée sur POWER AMC, Post gres")
+st.write("Géoreferencement de carte")
+st.write("Numérisation")
+st.write("Maitrise des techniques de collecte données:application Mobile Topographer,Kobo Toolbox")
+st.write("Conception  de Plans sur Autocad")
+st.write("Maitrise avancée du Pack Office (EXCEL pour le traitement de données topographiques, WORD pour la réedaction de rapports technique, POWER POINT pour la Presentation de projets")
+
+st.subheader("Perspectives")
+st.write("Ambitionne de créer un cabinet d'expertise en Géomatique spécialisé dans L'Amenagement du territoire")
+st.write("Ambitionne de concevoir et d'administrer des bases de données spatiales (post gis) pour l'analyse de données geographiques")
+st.write("Specialiste SiG")
